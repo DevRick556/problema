baf8dc447e7bb00a29facecebe59e54f53f99067
DevRick556 <rickpereiradequeiroz@gmail.com>
2024-12-11 18:41:30 -0400

    Mensagem descritiva do commit

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+const LoginPagw = () => {
+    return ( <h1>ola mundo</h1> );
+}
+ 
+export default LoginPagw;
---
+++ app/sales/page.tsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return ( 
+        <h1 className="">Home page</h1> //cor q o tailwind da pra nos
+     );
+}
+ 
+export default Home;
